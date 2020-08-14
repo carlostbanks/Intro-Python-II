b97e4f996546cc6c79bbad648d1b945069b6a396
Carlos Banks <cbanks@clypr.co>
2020-08-14 18:32:35 -0400

    started MVP 2 and added item to room.py

--- src/room.py
+++ src/room.py
@@ -9,3 +9,16 @@ class Room():
         self.s_to = None
         self.w_to = None
         self.e_to = None
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def pick_up_item(self, item_name):
+        for i in range(len(self.items)):
+            if self.items[i].name == item_name:
+                return self.items.pop(i)
+
+        return None
+
+
