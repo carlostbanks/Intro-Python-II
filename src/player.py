f10a1ae01d84e1ae320c0bc3f915f9719bd9e28e
Carlos Banks <cbanks@clypr.co>
2020-08-14 18:49:31 -0400

    added item to player.py

--- src/player.py
+++ src/player.py
@@ -4,6 +4,15 @@
 class Player():
     def __init__(self, original_room):
         self.current_room = original_room
+        self.items = []
 
     def move_to(self, new_room):
         self.current_room = new_room
+
+    def drop_item(self, item_name):
+        for i in range(len(self.items)):
+            if self.items[i].name == item_name:
+                item = self.items.pop()
+                item.drop()
+                self.current_room.add_item(item)
+        print(f"{item_name} does not exist in this room!")
